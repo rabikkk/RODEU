1699502251250c53bc5a70877d633d3efd6b9221
sametkarakoyunlu <53367353+sametkarakoyunlu@users.noreply.github.com>
2020-04-27 22:14:44 +0300

    Update LineFollower_v1_0.c

--- src/LineFollower_v1_0.c
+++ src/LineFollower_v1_0.c
@@ -7,7 +7,7 @@
 /***********************************************************************/
 /*   Allâhü lâ ilâhe illâ hüvel hayyül kayyûm, lâ te'huzühu sinetün velâ nevm,
  lehu mâ fissemâvâti ve ma fil'ard, men zellezi yeşfeu indehu illâ bi'iznih,
- ya'lemü mâ beyne eydiyhim vemâ halfehüm,
+ ya,
  velâ yü-hîtûne bi'şey'im min ilmihî illâ bima şâe vesia kürsiyyühüssemâvâti vel'ard,
  velâ yeûdühû hıfzuhümâ ve hüvel aliyyül azim.
  */
